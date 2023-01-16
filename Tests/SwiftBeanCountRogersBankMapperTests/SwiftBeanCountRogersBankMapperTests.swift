431ec624b577238bb98fc113b226effa5cb4daee
Steffen Kötte <steffen.koette@gmail.com>
2023-01-15 18:34:29 -0800

    Add tests

--- Tests/SwiftBeanCountRogersBankMapperTests/SwiftBeanCountRogersBankMapperTests.swift
+++ Tests/SwiftBeanCountRogersBankMapperTests/SwiftBeanCountRogersBankMapperTests.swift
@@ -98,18 +98,23 @@ final class SwiftBeanCountRogersBankMapperTests: XCTestCase {
         activity2.activityCategory = .purchase
         activity2.postedDate = Date()
         activity2.referenceNumber = "852741963"
+        var activity3 = TestActivity()
+        activity3.activityStatus = .approved
+        activity3.activityCategory = .overlimitFee
+        activity3.postedDate = Date()
         let amount = TestAmount(value: "2.79", currency: "USD")
         var foreign = TestForeignCurrency()
         foreign.originalAmount = amount
         activity2.foreign = foreign
         let mapper = SwiftBeanCountRogersBankMapper(ledger: ledger)
-        let result = try mapper.mapActivitiesToTransactions(activities: [activity1, activity2])
-        XCTAssertEqual(result.count, 2)
+        let result = try mapper.mapActivitiesToTransactions(activities: [activity1, activity2, activity3])
+        XCTAssertEqual(result.count, 3)
         var postings = [
             Posting(accountName: accountName, amount: SwiftBeanCountModel.Amount(number: Decimal(string: "-1.13")!, commoditySymbol: "CAD", decimalDigits: 2)),
             Posting(accountName: mapper.expenseAccountName, amount: SwiftBeanCountModel.Amount(number: Decimal(string: "1.13")!, commoditySymbol: "CAD", decimalDigits: 2))
         ]
         let metaData = [MetaDataKeys.activityId: "payment-\(Self.dateFormatter.string(from: activity1.postedDate!))"]
+        let metaData3 = [MetaDataKeys.activityId: "overlimit-fee-\(Self.dateFormatter.string(from: activity3.postedDate!))"]
         XCTAssertEqual(result[0],
                        Transaction(metaData: TransactionMetaData(date: activity1.postedDate!, narration: "Test Merchant Name", metaData: metaData), postings: postings))
         postings[1] = Posting(accountName: mapper.expenseAccountName,
@@ -117,6 +122,7 @@ final class SwiftBeanCountRogersBankMapperTests: XCTestCase {
                               price: SwiftBeanCountModel.Amount(number: Decimal(string: "1.13")!, commoditySymbol: "CAD", decimalDigits: 2))
         let transactionMetaData = TransactionMetaData(date: activity2.postedDate!, narration: "Test Merchant Name", metaData: [MetaDataKeys.activityId: "852741963"])
         XCTAssertEqual(result[1], Transaction(metaData: transactionMetaData, postings: postings))
+        XCTAssertEqual(result[2].metaData.metaData, metaData3)
     }
 
     func testMapActivityDuplicate() throws {
